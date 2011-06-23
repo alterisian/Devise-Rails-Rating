7993def57e9089a509ccf9f5f7bec1a29b912e85
Ian M <ian@alteris.co.uk>
2011-06-23 18:57:29 +0100

    adding app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Templateapp::Application.config.secret_token = '766d2842594f328ee3e8216754bb78a80dc9db01c9a12341ad90db4fd88d1fe93d3b42ad5acb5812d84eea60d2dcbb052222995f59df8550a3f9b15dd47500b7'
